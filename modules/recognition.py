e62630143542793e0201f578cacb64533d76bf8b
Luigi Cabrera <luigi.cabrera@ucsp.edu.pe>
2021-10-06 08:41:01 -0500

    Implementacion de google search, no como modulo, sino como metodo de willy. Mas sencillo y optimizado (SECOND TIME)

--- modules/recognition.py
+++ modules/recognition.py
@@ -1,3 +1,58 @@
+import pytesseract
+import subprocess
+import numpy as np
+import cv2
+import os
+import concurrent.futures
 
-def get_query():
-    return
+FIRST_COORDS = 574 # Y-coord of the upper-left corner of the question box
+LINE_HEIGHT = 48 # Height of each question line
+DISTANCE_QA = 49 # Distance from question to answer
+DISTANCE_AA = 115 # Distance from answer to answer
+ANSWER_HEIGHT = 58 # Height of each answer
+
+path = "C:\\Program Files (x86)\\adb\\platform-tools"
+os.chdir(path)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def get_screenshot(lines):
+    image_bytes = subprocess.Popen("adb shell screencap -p",
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE, shell=True).stdout.read().replace(b'\r\n', b'\n')
+    image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_GRAYSCALE)[FIRST_COORDS:,:]
+    '''SHOW IMAGES (SET UP SS COORDS)'''
+    for i in range(lines):
+        ss = image[(LINE_HEIGHT + 1) * i: LINE_HEIGHT + (LINE_HEIGHT + 1) * i, 4:-4]
+        for i in range(len(ss)):
+            for j in range(len(ss[0])):
+                ss[i][j] = 255 if ss[i][j] <100 else 0
+        #cv2.imshow("", ss)
+        #cv2.waitKey(0)
+    for i in range(3):
+        ss = image[(LINE_HEIGHT + 1)*lines + DISTANCE_QA + DISTANCE_AA*i : (LINE_HEIGHT + 1)*lines + DISTANCE_QA + DISTANCE_AA*i + ANSWER_HEIGHT, 55:665]
+        for i in range(len(ss)):
+            for j in range(len(ss[0])):
+                ss[i][j] = 255 if ss[i][j] <56 else 0
+        #cv2.imshow("", ss)
+        #cv2.waitKey(0)
+    return image
+    
+def simple_recognize(screenshot):
+    return pytesseract.image_to_string(screenshot, lang='spa').strip()
+
+def get_query(counter):
+    screenshot = get_screenshot(counter)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_q = [executor.submit(simple_recognize, screenshot[(LINE_HEIGHT + 1) * i: LINE_HEIGHT + (LINE_HEIGHT + 1) * i, 4:-4]) for i in range(counter)]
+        future_a = [executor.submit(simple_recognize, screenshot[(LINE_HEIGHT + 1)*counter + DISTANCE_QA + DISTANCE_AA*i : (LINE_HEIGHT + 1)*counter + DISTANCE_QA + DISTANCE_AA*i + ANSWER_HEIGHT, 55:665]) for i in range(3)]
+    
+    q_list = [f.result() for f in future_q]
+    question = " ".join(q_list)
+    answers = [f.result() for f in future_a]
+        
+    print("\nWilly read:\nQuestion: {}\nAnswers: {}\n".format(question, answers))
+    return question, answers
+    
+#lines = int(input("How many lines: "))
+# start_time = time.time()
+#get_query(lines)
--- willy.py
+++ willy.py
@@ -1,16 +1,55 @@
 from modules.globals import *
 from modules.recognition import get_query
 from modules.text_procesing import *
+from googleapiclient.discovery import build 
+from bs4.element import ResultSet
 import concurrent.futures
 
 class Willy:
     def __init__(self):
+
+        # Load keys
+        keys = open("modules//keys.txt", "r")
+        self.api_key = keys.readline()[:-1]
+        self.cse_id = keys.readline()
+        keys.close()
+
         self.no_in_question = False
         self.brute_question = None
         self.brute_answers = None
         self.token_question = None
         self.token_answers = None
         self.answer_key_words = None
+
+        self.search_titles = []
+        self.search_snippets = []
+        self.urls = []
+        self.exlude_urls = []
+        self.doc_name = ""
+
+    def reset(self):
+        self.search_titles = []
+        self.search_snippets = []
+        self.urls = []
+        self.exlude_urls = []
+
+
+    def google_search(self):
+
+        results = build("customsearch","v1",developerKey=self.api_key).cse().list(q=self.brute_question,cx=self.cse_id,num=5).execute()
+        if not results.get('items'): return 0 
+
+        for r in results:
+            self.search_titles.append(r['title'])
+            self.search_titles.append(r['snippet'])
+            url_low = r['link'].lower()
+            if url_low.find("youtube") == -1 and url_low.find("anexo") == -1 and url_low.find("archivo") == -1 and url_low.find("image") == -1:
+                self.urls.append(r['link'])
+                if len(self.urls) == 1: self.doc_name = r['title']
+            else: self.exlude_urls.append(r['link'])
+
+        if len(self.urls) == 0: return -1
+        return 1
     
     def run(self, q_lines):
 
@@ -25,7 +64,21 @@ class Willy:
             self.token_question = tq.result()
             self.token_answers = [exe.result() for exe in ta]
             self.answer_key_words = [(word for word in answ) for answ in self.token_answers]
-           
+        
+        # GOOGLE SEARCH
+        var = self.google_search()
+        if var == 0: # Nothing to do :(
+            print("No Google Results D:")
+            return 
+        
+        # Check answer in search, snippet, doc name and star final answer
+        
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            answ_search = executor.submit()
+            answ_snippet = executor.submit()
+            answ_doc_name = executor.submit()
+            answ_tf_idf = executor.submit()
+ 
 
         return
     
